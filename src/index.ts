5617a6ae5c006974b3c3dcecbe7a54244003d436
ashinjoy <ashinjoy22@gmail.com>
2025-01-16 23:13:39 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-
-import {app} from './app'
-import { secrets } from './constants/secrets';  
-import './config/dbconfig'
-import { connectRedis } from './config/redis';
-connectRedis()
-app.listen( secrets.PORT || 3000, () => console.log("server is running"));  
+import { app } from "./app";
+import { secrets } from "./constants/secrets";
+import "./config/dbconfig";
+import { connectRedis } from "./config/redis";
+connectRedis();
+app.listen(secrets.PORT || 3000, () => console.log("server is running"));
